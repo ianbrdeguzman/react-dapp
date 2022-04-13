b35655099c09a6a7a95a90942f61058326021f95
ianbrdeguzman <ianbrdeguzman@gmail.com>
2022-04-12 22:27:40 -0400

    read and write to the blockchain

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
 import './App.css';
 
-function App() {
+const greeterContractAddress = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
+
+export default function App() {
+  const [greeting, setGreeting] = useState<string>('');
+
+  const requestAccount = async () => {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  };
+
+  const handleGetGreeting = async () => {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      console.log('Provider:', provider);
+
+      const contract = new ethers.Contract(
+        greeterContractAddress,
+        Greeter.abi,
+        provider
+      );
+
+      console.log('Contract:', contract);
+      try {
+        const data = await contract.greet();
+        console.log('Data:', data);
+      } catch (error) {
+        console.log('Error:', error);
+      }
+    }
+  };
+
+  const handleSetGreeting = async () => {
+    if (!greeting) return;
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount();
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      console.log('Provider:', provider);
+
+      const signer = provider.getSigner();
+      console.log('Signer:', signer);
+
+      const contract = new ethers.Contract(
+        greeterContractAddress,
+        Greeter.abi,
+        signer
+      );
+      console.log('Contract:', contract);
+
+      const transaction = await contract.setGreeting(greeting);
+      console.log('Transaction:', transaction);
+
+      await transaction.wait();
+      handleGetGreeting();
+    }
+    setGreeting('');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          type="text"
+          onChange={(e) => setGreeting(e.target.value)}
+          value={greeting}
+        />
+        <div className="App-buttons">
+          <button onClick={handleGetGreeting}>Get Greeting</button>
+          <button onClick={handleSetGreeting}>Set Greeting</button>
+        </div>
       </header>
     </div>
   );
 }
-
-export default App;
