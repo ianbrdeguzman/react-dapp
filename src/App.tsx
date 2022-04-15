785488e0e8040fab9fce5c925c6ffc42d442198f
ianbrdeguzman <ianbrdeguzman@gmail.com>
2022-04-15 11:35:50 -0400

    added ERC20 token

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,26 @@ import { useState } from 'react';
 import { ethers } from 'ethers';
 import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
 import './App.css';
+import MyERC20Token from './artifacts/contracts/MyERC20Token.sol/MyERC20Token.json';
 
-const greeterContractAddress = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
+const greeterContractAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
+const myERC20TokenAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
 export default function App() {
   const [greeting, setGreeting] = useState<string>('');
+  const [amount, setAmount] = useState<string>('');
+  const [address, setAddress] = useState<string>('');
 
   const requestAccount = async () => {
     await window.ethereum.request({ method: 'eth_requestAccounts' });
   };
 
+  const getProvider = async () =>
+    new ethers.providers.Web3Provider(window.ethereum);
+
   const handleGetGreeting = async () => {
     if (typeof window.ethereum !== 'undefined') {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const provider = await getProvider();
       console.log('Provider:', provider);
 
       const contract = new ethers.Contract(
@@ -38,7 +45,7 @@ export default function App() {
     if (typeof window.ethereum !== 'undefined') {
       await requestAccount();
 
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const provider = await getProvider();
       console.log('Provider:', provider);
 
       const signer = provider.getSigner();
@@ -60,6 +67,38 @@ export default function App() {
     setGreeting('');
   };
 
+  const handleGetBalance = async () => {
+    if (typeof window.ethereum !== 'undefined') {
+      const [account] = await window.ethereum.request({
+        method: 'eth_requestAccounts'
+      });
+      const provider = await getProvider();
+      const contract = new ethers.Contract(
+        myERC20TokenAddress,
+        MyERC20Token.abi,
+        provider
+      );
+      const balance = await contract.balanceOf(account);
+      console.log('MyERC20Token balance: ', balance.toString());
+    }
+  };
+
+  const handleSendToken = async () => {
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount();
+      const provider = await getProvider();
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        myERC20TokenAddress,
+        MyERC20Token.abi,
+        signer
+      );
+      const transaction = await contract.transfer(address, amount);
+      await transaction.wait();
+      console.log(`You've successfully sent ${amount} MET to ${address}`);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -71,6 +110,23 @@ export default function App() {
         <div className="App-buttons">
           <button onClick={handleGetGreeting}>Get Greeting</button>
           <button onClick={handleSetGreeting}>Set Greeting</button>
+        </div>
+        <br />
+        <input
+          type="text"
+          onChange={(e) => setAddress(e.target.value)}
+          value={address}
+          placeholder="Address"
+        />
+        <input
+          type="text"
+          onChange={(e) => setAmount(e.target.value)}
+          value={amount}
+          placeholder="Amount"
+        />
+        <div className="App-buttons">
+          <button onClick={handleGetBalance}>Get Token Balance</button>
+          <button onClick={handleSendToken}>Send Token</button>
         </div>
       </header>
     </div>
